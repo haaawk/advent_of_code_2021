26a944d4cc345099131419b5fc2b9dd683cc7096
Piotr Jastrzębski <haaawk@gmail.com>
2021-12-06 15:31:29 +0100

    Day 6

---
+++ day6/6a.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+	let mut line = String::new();
+	io::stdin().read_line(&mut line).unwrap();
+	let mut stats = [0u128; 9];
+	for n in line.trim().split(",").map(|x| x.parse::<usize>().unwrap()) {
+		stats[n] += 1;
+	}
+	for _day in 0..80 {
+		let mut new_stats = [0u128; 9];
+		new_stats[6] += stats[0];
+		new_stats[8] += stats[0]; 
+		for idx in 0..8 {
+			new_stats[idx] += stats[idx + 1];
+		}
+		stats = new_stats;
+	}
+	println!("{}", stats.iter().sum::<u128>());
+}
