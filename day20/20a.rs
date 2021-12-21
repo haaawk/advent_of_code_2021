2fc5550ce7fd9541fc60e6294488782392bc9ed6
Piotr Jastrzębski <haaawk@gmail.com>
2021-12-21 11:53:18 +0100

    Day 20

---
+++ day20/20a.rs
@@ -0,0 +1,52 @@
+use std::io::{self, BufRead};
+
+fn to_index(b: &[[bool; 106]; 106], x: usize, y: usize) -> usize {
+	let mut res = 0;
+	for dx in 0..3 {
+		for dy in 0..3 {
+			res = 2 * res + (if b[x + dx - 1][y + dy - 1] { 1 } else { 0 });
+		}
+	}
+	res
+}
+
+fn main() {
+	let stdin = io::stdin();
+	let lines: Vec<String> = stdin.lock().lines().map(|l| l.unwrap().trim().to_string()).collect();
+
+	let mut algo = Vec::<bool>::with_capacity(lines[0].as_bytes().len());
+	for c in lines[0].as_bytes() {
+		algo.push(if *c == b'#' { true } else { false });
+	}
+
+	let mut b = [[false; 106]; 106];
+	
+	for row in 0..lines.len() - 2 {
+		for col in 0..lines[2].len() {
+			b[row + 3][col + 3] = lines[row + 2].as_bytes()[col] == b'#';
+		}
+	}
+
+	// Step 1 - edge positions are all '.'
+	let mut b2 = [[true; 106]; 106];
+	for row in 1..b.len() - 1 {
+		for col in 1..b[0].len() - 1 {
+			b2[row][col] = algo[to_index(&b, row, col)];	
+		}
+	}
+
+	// Step 2 - edge positions are all '#'
+	let mut b3 = [[false; 106]; 106];
+	for row in 1..b.len() - 1 {
+		for col in 1..b[0].len() - 1 {
+			b3[row][col] = algo[to_index(&b2, row, col)];
+		}
+	}
+	let mut res = 0u64;
+	for row in 0..b.len() {
+		for col in 0..b[0].len() {
+			res += if b3[row][col] { 1 } else { 0 };
+		}
+	}
+	println!("{}", res);
+}
